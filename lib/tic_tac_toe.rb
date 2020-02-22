e1362abd925c882995baac10e7138da6328cbb26
Michaylah Mam <mchylh@gmail.com>
2020-02-22 05:32:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -118,7 +118,7 @@ end
 def draw?(board)
    if !won?(board) && full?(board)
      return true
-   elsif !won?(board) && !full?(board)
+   elsif !full?(board) && !won?(board)
      return false
    else won?(board)
      return false

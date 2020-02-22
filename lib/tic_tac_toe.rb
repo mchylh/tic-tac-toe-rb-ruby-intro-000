c56e934a5556e94a36af2a40bc3b785cab18f046
Michaylah Mam <mchylh@gmail.com>
2020-02-22 03:37:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,3 +12,53 @@ WIN_COMBINATIONS = [
 [0,4,8], # left_diagonal
 [6,4,2] # right_diagonal
 ]
+
+#build structure of the board
+puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+#display_board method
+def display_board
+   puts row = ["   " "|" "   " "|" "   "]
+   puts separator = "-----------"
+   puts row
+   puts separator
+   puts row
+ end
+ 
+#prints arbitrary arrangements of the board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(user_input)
+  user_input.to_i-1
+end
+
+# define move method
+def move(board, position, char="X")
+  board[position] = char
+end
+
+# code your #position_taken? method here!
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method below.
+def position_taken? (board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+#valid move
+# code your #valid_move? method here
+def valid_move?(board, index)
+   if index.between?(0,8) && !position_taken?(board, index)
+        puts "this is a valid move"
+      return true
+    else
+      return false
+   end
+end
+
+

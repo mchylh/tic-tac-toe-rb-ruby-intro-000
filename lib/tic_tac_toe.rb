764eec7f7a6a4d8ad7dfec2827e3be985d96e026
Michaylah Mam <mchylh@gmail.com>
2020-02-22 05:09:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,7 +45,11 @@ def move(board, index, player)
 end
 
 def position_taken? (board, index)
-  !(board[index].nil? || board[index] == " ")
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
 end
 
 #define current player

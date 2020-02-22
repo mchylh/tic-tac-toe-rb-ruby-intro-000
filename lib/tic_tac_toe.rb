e1a0b4054fea6bf4b53d74621e496d6ba136b0e6
Michaylah Mam <mchylh@gmail.com>
2020-02-22 03:45:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -91,6 +91,56 @@ def turn_count(board)
    end
    counter
 end
+
+#define current player
 def current_player(board)
    turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+#winning combo
+def won?(board)
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      position_1 = board[win_index_1] # value of board at win_index_1
+      position_2 = board[win_index_2] # value of board at win_index_2
+      position_3 = board[win_index_3] # value of board at win_index_3
+      if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+        return win_combination
+      end
+    end
+    return false
+end
+#position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+#The above code means to return first element (position_1) & make sure the position is taken by X or O
+
+#define full board
+def full?(board)
+   board.all? {|i| i == "X" || i == "O"}
+end
+
+#draw method for tie game
+def draw?(board)
+   if !won?(board) && full?(board)
+     return true
+   elsif !won?(board) && !full?(board)
+     return false
+   else won?(board)
+     return false
+   end
+end
+
+#end game method if draw or won or full
+def over?(board)
+   if draw?(board) || won?(board) || full?(board)
+     return true
+   end
+end
+
+#determine the winner
+def winner(board)
+   if won?(board)
+      return board[won?(board)[0]]
+   end
 end
